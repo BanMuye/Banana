17af48dcad9cb895e49f30cb72c64d65efcac1d7
zhouchunyang <2609714703@qq.com>
2025-09-08 21:23:16 +0800

    add scale to shader

--- Sandbox/src/SandboxApp.cc
+++ Sandbox/src/SandboxApp.cc
@@ -5,6 +5,7 @@
 #include "Banana/Core/Input.h"
 #include "Banana/Core/KeyCodes.h"
 #include "Banana/Renderer/Renderer.h"
+#include "glm/gtx/transform.hpp"
 #include "spdlog/spdlog.h"
 
 class ExampleLayer : public Banana::Layer {
@@ -42,6 +43,7 @@ public:
             layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec4 v_Position;
             out vec4 v_Color;
@@ -49,7 +51,7 @@ public:
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * a_Position;
+				gl_Position = u_ViewProjection * u_Transform * a_Position;
                 v_Color = a_Color;
 			}
 		)";
@@ -98,13 +100,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -126,18 +129,18 @@ public:
 
     void OnUpdate(const Banana::Timestep timestep) override {
         if (Banana::Input::IsKeyPressed(BANANA_KEY_LEFT))
-            m_CameraPosition.x += m_CameraMoveSpeed * timestep;
-        else if (Banana::Input::IsKeyPressed(BANANA_KEY_RIGHT))
             m_CameraPosition.x -= m_CameraMoveSpeed * timestep;
+        else if (Banana::Input::IsKeyPressed(BANANA_KEY_RIGHT))
+            m_CameraPosition.x += m_CameraMoveSpeed * timestep;
         if (Banana::Input::IsKeyPressed(BANANA_KEY_UP))
-            m_CameraPosition.y -= m_CameraMoveSpeed * timestep;
-        if (Banana::Input::IsKeyPressed(BANANA_KEY_DOWN))
             m_CameraPosition.y += m_CameraMoveSpeed * timestep;
+        if (Banana::Input::IsKeyPressed(BANANA_KEY_DOWN))
+            m_CameraPosition.y -= m_CameraMoveSpeed * timestep;
 
         if (Banana::Input::IsKeyPressed(BANANA_KEY_A))
-            m_CameraRotation -= m_CameraRotationSpeed * timestep;
-        if (Banana::Input::IsKeyPressed(BANANA_KEY_D))
             m_CameraRotation += m_CameraRotationSpeed * timestep;
+        if (Banana::Input::IsKeyPressed(BANANA_KEY_D))
+            m_CameraRotation -= m_CameraRotationSpeed * timestep;
 
         m_Camera.SetPosition(m_CameraPosition);
         m_Camera.SetRotation(m_CameraRotation);
@@ -146,7 +149,18 @@ public:
         Banana::RenderCommand::Clear();
 
         Banana::Renderer::BeginScene(m_Camera);
-        Banana::Renderer::Submit(blue_Shader, m_SquareVA);
+
+        glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1));
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                glm::vec3 pos = glm::vec3(i * 0.21, j * 0.21, 0.0f);
+                glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+
+                Banana::Renderer::Submit(blue_Shader, m_SquareVA, transform);
+            }
+        }
+
+
         Banana::Renderer::Submit(m_Shader, m_RectangleVA);
         Banana::Renderer::EndScene();
     }
